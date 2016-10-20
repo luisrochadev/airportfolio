0ed11e8a93571beb181643118a3e965fc60af900
Luis Bernardo Rocha <luisrocha@Luiss-MacBook-Pro-2.local>
2016-10-20 15:08:44 -0700

    changed facebook login route

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ var knex = require('../db/knex');
 passport.use(new FacebookStrategy({
 	clientID:"1700367380186405",
 	clientSecret:"e51788f34365250fc6d520d92ae648d5",
-	callbackURL: "https://airportfolio.herokuapp.com/auth/facebook/callback",
+	callbackURL: "http://localhost:3000/auth/facebook/callback",
 	profileFields: ['id', 'name', 'photos', 'emails']
 },
 function(accessToken, refreshToken, profile, done) {
@@ -27,7 +27,7 @@ router.get('/facebook', passport.authenticate('facebook', { scope: 'email'}));
 router.get('/facebook/callback', passport.authenticate('facebook', { 
 									successRedirect: '/auth/success',
                                     failureRedirect: '/' }));
-router.get('/auth/success', function(req, res, next){
+router.get('/success', function(req, res, next){
 	var id = req.session.passport.user.id;
 	knex('users').where({userID: id}).then(function(rows){
 		if(rows.length === 0){
