44d9def687d8140c710fbf5a1aabe0e17d49c6de
Luis Bernardo Rocha <luisrocha@Luiss-MacBook-Pro-2.local>
2016-10-20 15:26:07 -0700

    heroku postgres database create

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
   production: {
     client: 'postgres',
     connection: {
-    	database: 'meetup'
+    	database: 'postgres://khfkddtflrnpxu:a9z9twNRlF7w-x6Zpra4w5X9qA@ec2-54-243-63-130.compute-1.amazonaws.com:5432/dbobavr3ktlfd5'
     }
   }
 }
