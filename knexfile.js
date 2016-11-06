95d44542a37580b419c59f5f7a1ffd3e2058280e
Luis Rocha <luisrocha@luiss-mbp.gateway.sonic.net>
2016-11-05 17:13:20 -0700

    knex production settings

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgres',
+    client: 'postgresql',
     connection: {
     	database: 'postgres://khfkddtflrnpxu:a9z9twNRlF7w-x6Zpra4w5X9qA@ec2-54-243-63-130.compute-1.amazonaws.com:5432/dbobavr3ktlfd5'
     }
